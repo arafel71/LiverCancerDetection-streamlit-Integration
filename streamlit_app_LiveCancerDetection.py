fef58ebe00404da446b1ca138305533fb80caec9
arafel71 <olbergeron@yahoo.fr>
2023-04-30 19:29:35 +0200

    Add files via upload

--- streamlit_app_LiveCancerDetection.py
+++ streamlit_app_LiveCancerDetection.py
@@ -19,10 +19,11 @@ Image.MAX_IMAGE_PIXELS = 1000000000
 
 
 from torchvision import models
+from torchvision import transforms
 import torch
  
 #dir(models)
-st.write(dir(models))
+#st.write(dir(models))
 
 directory = os.getcwd()
 
@@ -89,8 +90,39 @@ if uploaded_file is not None:
     os.remove(os.path.join(pathtempDir,uploaded_file.name))
 
 
-   
+    ### TO DO call the model ont the button click on a specific image
+    alexnet = models.alexnet(pretrained=True)
+    st.write("details Alexnet models")
+    st.write(alexnet)
 
+
+    transform = transforms.Compose([            #[1]
+       transforms.Resize(256),                    #[2]
+       transforms.CenterCrop(224),                #[3]
+       transforms.ToTensor(),                     #[4]
+       transforms.Normalize(                      #[5]
+       mean=[0.485, 0.456, 0.406],                #[6]
+       std=[0.229, 0.224, 0.225]                  #[7]
+       )])
+
+    # open test image 
+    img = Image.open("dog.jpg")
+
+
+    # image transformation ,pre-process the image and prepare a batch to be passed through the network.
+    img_t = transform(img)
+    batch_t = torch.unsqueeze(img_t, 0)
+
+    # before inference, we need to put the model in eval mode
+
+    alexnet.eval()
+
+    # inference prediction
+
+    out = alexnet(batch_t)
+    st.write(out.shape)
+
+    #**************************
     #for archive
     #scene = slide.get_scene(0)   
 
