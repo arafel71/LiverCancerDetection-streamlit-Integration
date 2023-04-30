8c0df6e7de1bc10009ca4f36a93825596c05e0ce
arafel71 <olbergeron@yahoo.fr>
2023-04-30 15:59:06 +0200

    Add files via upload

--- streamlit_app_LiveCancerDetection.py
+++ streamlit_app_LiveCancerDetection.py
@@ -25,7 +25,7 @@ pathtempDir = os.path.join(directory + r'//tempDir/')
 
 
 
-st.markdown(f'<h1 style="color:#ffffff;background-color:#Eaa7a6;font-size:24px;">{"Liver Cancer Detection Application”"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:#ffffff;background-color:#Eaa7a6;font-size:48px;">{"  Liver Cancer Detection Application"}</h1>', unsafe_allow_html=True)
 
 
 """
